c890b1f00ce43375feb2a208ca325ed63e141e16
David Roy <david.roy@cabbit.co.uk>
2017-05-06 13:02:19 +0100

    adds forum posts skaffold

---
+++ test/system/posts_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class Forum::PostsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit forum_posts_url
+  #
+  #   assert_selector "h1", text: "Forum::Post"
+  # end
+end
